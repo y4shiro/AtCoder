4006cd9e11b4285a965c2c8243020c07189f7b91
y4shiro <y4shironao@gmail.com>
2020-04-19 22:12:35 +0900

    C問題

---
+++ abc/163/c.js
@@ -0,0 +1,25 @@
+// let input = `5
+// 1 1 2 2`;
+
+let input = `10
+1 1 1 1 1 1 1 1 1`;
+
+// let input = `7
+// 1 2 3 4 5 6`;
+
+// function Main(input) {
+function Main() {
+  // 文字列をsplitして配列に格納
+  input = input.split("\n");
+  tmp1 = input[0].split(" ");
+  tmp2 = input[1].split(" ");
+
+  let n = parseInt(tmp1[0], 10);
+  let staff = tmp2.map(e => parseInt(e));
+
+  for (let i = 1; i <= n; i++){
+    console.log(staff.filter(function(x){return x==i}).length);
+  }
+}
+// Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+Main();
