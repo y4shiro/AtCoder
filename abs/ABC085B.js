61df16fdf13f4b49b4525723ea283288330cf4b4
y4shiro <y4shironao@gmail.com>
2020-04-23 00:01:57 +0900

    ABC085B 本番で WA 出たので修正

--- abs/ABC085B.js
+++ abs/ABC085B.js
@@ -9,18 +9,14 @@ const input=`7
 
 function Main(input){
   // 文字列を split 後、Int に変換して格納
+  input = input.trim();
   input = input.split("\n");
-  const n = parseInt(input.shift());
-  let mochi = input.map(e => parseInt(e));
+  input.shift();
 
   // 重複を弾く
-  let set = new Set(mochi);
-  mochi = [...set];
+  let set = new Set(input);
 
-  // 降順にソートする
-  mochi.sort((a, b) => b - a);
-
-  console.log(mochi.length);
+  console.log(set.size);
 };
 
 // Main(require("fs").readFileSync("/dev/stdin", "utf8"));
